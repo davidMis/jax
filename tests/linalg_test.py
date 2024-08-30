031dd9c90c91987a695ec7534f33755b5c001f63
Peter Hawkins <phawkins@google.com>
2024-08-29 17:59:40 -0700

    Relax test tolerance to fix a test failure on ARM CPU.
    PiperOrigin-RevId: 669125669

--- tests/linalg_test.py
+++ tests/linalg_test.py
@@ -325,7 +325,7 @@ class NumpyLinalgTest(jtu.JaxTestCase):
     a, = args_maker()
     w1, _ = jnp.linalg.eig(a)
     w2 = jnp.linalg.eigvals(a)
-    self.assertAllClose(w1, w2, rtol={np.complex64: 1e-5, np.complex128: 1e-14})
+    self.assertAllClose(w1, w2, rtol={np.complex64: 1e-5, np.complex128: 2e-14})
 
   @jtu.run_on_devices("cpu")
   def testEigvalsInf(self):
